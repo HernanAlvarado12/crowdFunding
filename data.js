a04cd574c3ccd4dc358083ee5434570db9c1b8d1
halvaradop <hernanvid123@gmail.com>
2023-02-11 15:54:41 -0500

    fixed error

---
+++ data.js
@@ -0,0 +1,54 @@
+const backPlan = [
+    {
+        title: 'Pledge with no reward',
+        subtitle: '',
+        content: `Choose to support us without a reward if you simply believe in our 
+        project. As a backer, you will be signed up to receive product updates 
+        via email.`,
+        number: '',
+        state: true
+    },
+    {
+        title: 'Bamboo Stand',
+        subtitle: 'Pledge $25 or more',
+        content: `You get an ergonomic stand made of natural bamboo. You've helped us 
+        launch our promotional campaign, and you’ll be added to a special Backer 
+        member list.`,
+        number: '101',
+        state: true
+    },
+    {
+        title: 'Back Edition Stand',
+        subtitle: 'Pledge $75 or more',
+        content: `You get a Black Special Edition computer stand and a personal thank you. 
+        You’ll be added to our Backer member list. Shipping is included.`,
+        number: '64',
+        state: true
+    },
+    {
+        title: 'Mahogany Special Edition',
+        subtitle: 'Pledge $200 or more',
+        content: `You get two Special Edition Mahogany stands, a Backer T-Shirt, and a
+        personal thank you. You’ll be added to our Backer member list. Shipping 
+        is included.`,
+        number: '0',
+        state: false
+    }
+]
+
+const projects = [
+    {
+        title: '$89,914',
+        subtitle: 'of $100.00 backed'
+    },
+    {
+        title: '5,007',
+        subtitle: 'total backers'
+    },
+    {
+        title: '56',
+        subtitle: 'days left'
+    }
+]
+
+export { backPlan, projects }
--- model.js
+++ model.js
@@ -1,4 +1,13 @@
+import { backPlan, projects } from './data.js'
+
+
+const planFragment = document.createDocumentFragment()
+const backFragment = document.createDocumentFragment()
 const dialog = document.querySelector('dialog.max-w-3xl')
+const projectsFragment = document.createDocumentFragment()
+const planTemplate = document.getElementById('planTemplate').content
+const backTemplate = document.getElementById('backplanTemplate').content
+const projectTemplate = document.getElementById('projectTemplate').content
 
 
 document.addEventListener('click', event => {
@@ -18,3 +27,40 @@ document.addEventListener('click', event => {
         dialog.close()
     }
 })
+
+
+backPlan.forEach(plan => {
+    const { title, subtitle, content, state} = plan
+    const cloneNode = document.importNode(backTemplate, true)
+    cloneNode.querySelector('div.w-full > div > h3').textContent = title
+    cloneNode.querySelector('div.w-full > div > p').textContent = subtitle
+    cloneNode.querySelector('div.w-full + p').textContent = content
+    !state? cloneNode.firstElementChild.classList.add('opacity-60') : ''
+    backFragment.append(cloneNode)
+})
+document.querySelector('nav + dialog > section').append(backFragment)
+
+projects.forEach(project => {
+    const { title, subtitle} = project
+    const cloneNode = document.importNode(projectTemplate, true)
+    cloneNode.querySelector('h3.text-black').textContent = title
+    cloneNode.querySelector('p.text-gray').textContent = subtitle
+    projectsFragment.append(cloneNode)
+})
+
+const line = document.querySelector('#projectTemplate + span')
+document.querySelector('section.my-5.px-2.flex-flow').insertBefore(projectsFragment, line)
+
+
+backPlan.slice(1).forEach(plan => {
+    const { title, subtitle, content, state, number} = plan
+    const cloneNode = document.importNode(planTemplate, true)
+    cloneNode.querySelector('div > h4.font-semibold').textContent = title
+    cloneNode.querySelector('div > p.font-medium').textContent = subtitle
+    cloneNode.querySelector('div + p').textContent = content
+    cloneNode.querySelector('div.flex > span.text-base').textContent = number
+    !state? cloneNode.firstElementChild.classList.add('opacity-60') : ''
+    planFragment.append(cloneNode)
+})
+document.querySelector('main > section > section').append(planFragment)
+
